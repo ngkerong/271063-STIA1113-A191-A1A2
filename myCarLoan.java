aacbbc2b82d3d61c8480edca17f1477c6b21ab48
ngkerong <55265526+ngkerong@users.noreply.github.com>
2019-11-07 23:53:50 +0800

    Update myCarLoan.java

--- myCarLoan.java
+++ myCarLoan.java
@@ -8,12 +8,10 @@ public class myCarLoan {
     public static void main(String[] args){
         
         Scanner input = new Scanner(System.in);
-        
-        String format = "\n-%20s-%25s-%23s-%17s\n";
         float A,B,C,D;
         
         /*Display*/
-        System.out.println("CAR LOAN CALCULATOR");
+        System.out.println("CAR LOAN CALCULATOR\n");
         
         /*Input Car Loan,Down Payment, Loan Period and Interest Rate*/
         System.out.print("Car Price(RM):");
@@ -26,12 +24,13 @@ public class myCarLoan {
         D = input.nextFloat();
         
         /*Looping*/
-        if(A >= 30000.00 & B >= 0.00 & C >= 5.00 & C <= 9.00 & D >= 3.00 & D <= 7 ){
+        if(A >= 30000.00 & B >= 0.00 & C >= 5.00 & C <= 9.00 & D >= 3.00 & D <= 7.00 ){
             
             /*Call Method*/
             CalculateLoan(A,B,C,D);
 
         }else{
+            /*Display Error*/
             System.out.println("Error, please enter again.");
         }
         
@@ -41,10 +40,10 @@ public class myCarLoan {
     public static void CalculateLoan(float A, float B, float C, float D ){
         
         float Interest,Principal,Repayment,TotalPrinc=0,TotalInte=0,Balance=0;
-        String format1 = "%10s-%15s-%15s-%15s\n",
-        format2 = "%40s\n%28s%.2f\n\n",
-        format3 = "%9s%17.2f%16.2f%17.2f\n";
         String[] Years = {"1st","2nd","3rd","4th","5th","6th","7th","8th"};
+        String format1 = "%40s\n%28s%.2f\n\n",
+        format2 = "%10s-%15s-%15s-%15s\n",
+        format3 = "%9s%17.2f%16.2f%17.2f\n";
         byte i = 0;
         
         
@@ -54,10 +53,10 @@ public class myCarLoan {
         Repayment = Principal /12;
         
         /*Display Repayment*/
-        System.out.printf(format2, "Monthly Repayment","RM",Repayment);
+        System.out.printf(format1, "Monthly Repayment","RM",Repayment);
         
-        /*Display Years, Principal, Interest, Balance*/
-        System.out.printf(format1, "YEARS", "PRINCIPAL", "INTEREST", "BALANCE");
+        /*Display Result*/
+        System.out.printf(format2, "YEARS", "PRINCIPAL", "INTEREST", "BALANCE");
         
         /*Looping*/
         while(C != 0){
